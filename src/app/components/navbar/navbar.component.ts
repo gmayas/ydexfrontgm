cc5f4fb88f9b0d294e8c02447a5dd38dd964d5eb
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2021-09-28 13:14:02 -0500

    Router Navigate commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  /*
+  { path: '', component:  HomeComponent },
+  { path: 'alg01', component: Algoritmo01Component  },
+  { path: 'alg02', component: Algoritmo02Component  },
+  { path: 'users', component: UsersComponent  },
+  { path: 'userdata', component: UserdataComponent  },
+  { path: 'about', component: AboutComponent  },
+   */
+  // 
+  goHome() {
+      try {
+        this.router.navigate(['']);
+      } catch (e) {
+        console.log('Error respose: ', e)
+      }
+  }
+  //
+  goAlg01() {
+    try {
+      this.router.navigate(['alg01']);
+    } catch (e) {
+      console.log('Error respose: ', e)
+    }
+  }
+
+  //
+  goAlg02() {
+    try {
+      this.router.navigate(['alg02']);
+    } catch (e) {
+      console.log('Error respose: ', e)
+    }
+  }
+
+  // 
+  goUsers() {
+    try {
+      this.router.navigate(['users']);
+    } catch (e) {
+      console.log('Error respose: ', e)
+    }
+  }
+
+  //
+  goAbout() {
+    try {
+      this.router.navigate(['about']);
+    } catch (e) {
+      console.log('Error respose: ', e)
+    }
   }
 
 }
